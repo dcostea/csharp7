c44161e5e9bb1191a78f6e6a8d02de84cc21d0fa
Daniel Costea <daniel_costea@ymail.com>
2017-05-05 01:21:24 +0300

    Simplify code from T to int[]

--- TupleSelectSort/Program.cs
+++ TupleSelectSort/Program.cs
@@ -16,52 +16,34 @@ namespace TupleSelectSort
             WriteLine(string.Join<int>(",", a));
             WriteLine(string.Join<int>(",", Sort(a)));
 
-            double[] b = { 5.1, 7.1, 8.1, 3.1, 1.1, 2.1, 6.1 };
 
-            WriteLine(string.Join<double>(",", b));
-            WriteLine(string.Join<double>(",", Sort(b)));
-
-            string[] c = { "ccc", "ddd", "aaa", "bbb", "xxx" };
-
-            WriteLine(string.Join<string>(",", c));
-            WriteLine(string.Join<string>(",", Sort(c)));
-
-
-            ReadKey();
+            ReadKey(true);
         }
 
-        private static void SwapIfGreater<T>(ref T x, ref T y)
+        private static int[] Sort(int[] a)
         {
-            T temp;
-            if (((IComparable)x).CompareTo(y) > 0)
-            {
-                temp = x;
-                x = y;
-                y = temp;
-            }
-        }
-
-        private static T[] Sort<T>(T[] a)
-        {
-            T max;
+            int max;
             int pos;
 
             for (int i = 0; i < a.Length; i++)
             {
                 (max, pos) = GetMax(a, i);
-                SwapIfGreater(ref a[i], ref a[pos]);
+                if (a[i] > a[pos])
+                {
+                    (a[i], a[pos]) = (a[pos], a[i]); // swap elements using tuples
+                }
             }
 
             return a;
 
-            (T max, int pos) GetMax(T[] t, int index)
+            (int max, int pos) GetMax(int[] t, int index)
             {
-                T m = t[index];
+                int m = t[index];
                 var p = index;
 
                 for (int i = index; i < a.Length; i++)
                 {
-                    if (((IComparable)m).CompareTo(a[i]) > 0)
+                    if (m > a[i])
                     {
                         m = a[i];
                         p = i;
